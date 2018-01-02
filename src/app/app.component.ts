70f4a66822a0952b5a39c0ff80e3623d8a97b7c3
wan muzaffar Wan Hashim <wanmuz86@gmail.com>
2018-01-02 21:51:38 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Muzaffar app';
+  number = 0;
+  message = '';
+  items = [{
+  	name:"Learn Angular",
+  	place:"ADA",
+  	completed:true
+  },
+  {
+  	name:"Have Dinner",
+  	place:"KFC",
+  	completed:false
+  },
+  {
+  	name:"Watch Movies",
+  	place:"Jaya One",
+  	completed:false
+  }
+  ]
+
+ngOnInit(){
+	console.log('page loaded');
+}
+
+newItem = {
+	name:'',
+	place:'',
+	description:'',
+	completed:false
+}
+selectedItem = {
+	name:'',
+	place:'',
+	description:'',
+	completed:false
+}
+  increment(){
+  	this.number++;
+  	this.message = '';
+  }
+  decrement(){
+  	if (this.number >0){
+  	this.number--;
+  	}
+  	else {
+  		this.message ='Number cannot be smaller than 0';
+  	}
+  }
+
+  reset(){
+  	this.number = 0;
+  	this.message = '';
+  }
+  savePressed(){
+  	this.items.push(this.newItem);
+  	this.newItem = {
+	name:'',
+	place:'',
+	description:'',
+	completed:false
+}
+  }
+  itemSelected(item){
+  	console.log(item);
+  	this.selectedItem = item;
+  }
 }
